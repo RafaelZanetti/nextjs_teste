f32f4ba5e2ac5370218fe61d4b31f0b95b78bfc3
unknown <rafa_zanetti_c@hotmail.com>
2020-11-10 15:15:28 -0300

    primeira pagina com NextJS

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Contador from './components/contador'
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+            <Contador />
+        </div>
+    )
+}
+
+export default Home;
